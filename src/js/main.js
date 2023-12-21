2efe182ec44c1c7852d92766fac4652baad7eed0
ChurrinChurron <marianafantasy8@gmail.com>
2023-12-21 03:50:13 -0400

    arreglo galeria certif

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 let btnMenu = document.querySelector("#btnNav");
 let btnCerrar = document.querySelector("#btnCer");
 let menuMob = document.querySelector("#menuMob");
+let sliderimagenes = document.querySelectorAll(".galeria__img img");
+let izquierda = document.querySelector("#btnIzq");
+let derecha = document.querySelector("#btnDer");
+let current = 0;
+let certifTit = document.querySelector("#certificTit");
+
 
 btnMenu.addEventListener("click", () => {
 
@@ -12,54 +18,58 @@ btnCerrar.addEventListener("click", () => {
     menuMob.classList.remove("abrir");
 });
 
+function reset() {
+for (let i = 0; i < sliderimagenes.length; i++) {
+  sliderimagenes[i].style.display = "none";
 
-
-let slider = document.querySelector(".galeria__img");
-let sliderImg = document.querySelectorAll(".galeria__img img");
-let sliderIzq = document.querySelector("#btnIzq");
-let sliderDer = document.querySelector("#btnDer");
-let contador = 1;
-let tamañoSlider = sliderImg[0].clientWidth;
-let certifTit = document.querySelector("#certificTit");
-
-/*FUNCION SLIDER*/
-
-slider.style.transform = `translateX(${-tamañoSlider * contador}px)`;
-
-sliderDer.addEventListener("click", () => {
-
-    slider.style.transition = "none";
-    contador++;
-    slider.style.transform = `translateX(${-tamañoSlider * contador}px)`;
-})
-
-sliderIzq.addEventListener("click", () => {
-
-    slider.style.transition = "none";
-    contador--;
-    slider.style.transform = `translateX(${-tamañoSlider * contador}px)`;
-})
-
-slider.addEventListener("transitionend", () => {
-
-    if(sliderImg[contador].id === "ultimaImg") {
-
-        slider.style.transition = "none";
-        contador = sliderImg.length - 2;
-        slider.style.transform = `translateX(${-tamañoSlider * contador}px)`;
-        certifTit.innerHTML = "Certificado Autodesk -";
     }
+}
 
-    if(sliderImg[contador].id === "primeraImg") {
+function startSlide() {
+reset();
+sliderimagenes[0].style.display = "block";
+}
 
-        slider.style.transition = "none";
-        contador = sliderImg.length - contador;
-        slider.style.transform = `translateX(${-tamañoSlider * contador}px)`;
-        certifTit.innerHTML = "Certificado ECIC -";
+function slideLeft() {
+reset();
+sliderimagenes[current - 1].style.display = "block";
+current--;
+}
+
+function slideRight() {
+reset();
+sliderimagenes[current + 1].style.display = "block";
+current++;
+}
+
+function changeText() {
+
+    if(current === 0) {
+
+        certifTit.innerHTML = "Certificado AutoDesk -"
+    } else if (current === 1) {
+    
+        certifTit.innerHTML = "Certificado Cype -"
+    } else {
+    
+        certifTit.innerHTML = "Certificado ECIC -"
     }
+}
 
-    else {
+izquierda.addEventListener("click", function () {
+if (current === 0) {
+  current = sliderimagenes.length;
+}
+slideLeft();
+changeText();
+});
 
-        certifTit.innerHTML = "Certificado Cype -";
-    }
-})
+derecha.addEventListener("click", function () {
+if (current === sliderimagenes.length - 1) {
+  current = -1;
+}
+slideRight();
+changeText();
+});
+
+startSlide();
